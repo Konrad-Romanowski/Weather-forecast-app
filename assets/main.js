4647280e3334d21a602c2c3c409a48e61e738643
Konrad Romanowski <konrad.romanowski.it@gmail.com>
2021-12-29 16:42:47 +0100

    added basic fetch functionality to the client's side logic

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,14 @@
-console.log("Hello world in the client's console");
+const searchBtn = document.getElementById('submit-button');
+const cityNameInput = document.getElementById('search-input');
+let cityName;
+
+searchBtn.addEventListener('click',e=>{
+    e.preventDefault();
+
+    cityName = cityNameInput.value;
+    if(cityName.length < 1) return;
+    cityNameInput.value = '';
+
+    console.log("Search button clicked!", cityName);
+    fetch(`/weather/${cityName}`);
+});
