4c4ab15cd798b5645492c076f606a3cc96bfb2dc
Konrad Romanowski <konrad.romanowski.it@gmail.com>
2022-01-04 18:31:48 +0100

    Refactored server-side code

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const getWeatherData = require('./getWeatherData');
 
-const fetch = require('node-fetch');
 const dotenv = require('dotenv');
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
-
-const api_key = 'your api key here';
-const API_KEY = process.env.API_KEY || api_key;
 
 app.use('/public',express.static(path.join(__dirname,'assets')));
 
@@ -19,19 +16,7 @@ app.get('/',(req,res)=>{
 
 app.get('/weather/:cityName', async (req,res)=>{
     const cityName = req.params.cityName;
-
-    const fetchData = await fetch(`http://api.openweathermap.org/data/2.5/weather?&units=metric&q=${cityName}&APPID=${API_KEY}`);
-    const readableData = await fetchData.json();
-
-    const weatherData = {
-        cityName: readableData.name,
-        currentDate: readableData.dt,
-        currentTemp: readableData.main.temp,
-        currentMinTemp: readableData.main.temp_min,
-        currentMaxTemp: readableData.main.temp_max,
-        currentWeatherDescription: readableData.weather[0].description,
-        currentWeatherIcon: readableData.weather[0].icon
-    }
+    const weatherData = await getWeatherData(cityName);
 
     res.send(weatherData);
 });
---
+++ getWeatherData.js
@@ -0,0 +1,25 @@
+const fetch = require('node-fetch');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const api_key = 'your api key here';
+const API_KEY = process.env.API_KEY || api_key;
+
+async function getWeatherData(cityName) {
+    const fetchData = await fetch(`http://api.openweathermap.org/data/2.5/weather?&units=metric&q=${cityName}&APPID=${API_KEY}`);
+    const readableData = await fetchData.json();
+
+    const weatherData = {
+        cityName: readableData.name,
+        currentDate: readableData.dt,
+        currentTemp: Math.round(readableData.main.temp),
+        currentMinTemp: Math.round(readableData.main.temp_min),
+        currentMaxTemp: Math.round(readableData.main.temp_max),
+        currentWeatherDescription: readableData.weather[0].description,
+        currentWeatherIcon: readableData.weather[0].icon
+    }
+
+    return weatherData;
+}
+
+module.exports = getWeatherData
