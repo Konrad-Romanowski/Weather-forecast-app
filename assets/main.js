79f14237ddc78b08331f1954382cc6ef00c3b4af
Konrad Romanowski <konrad.romanowski.it@gmail.com>
2022-01-06 23:31:03 +0100

    Weather data is now fetched on page load

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,11 @@
 const searchBtn = document.getElementById('submit-button');
 const cityNameInput = document.getElementById('search-input');
-let cityName;
+let cityName = localStorage.getItem('cityName') || 'Warsaw';
+
+window.onload = async function (){
+    const weatherData = await getWeatherData(cityName);
+    populateTemplate(weatherData);
+}
 
 searchBtn.addEventListener('click',async e=>{
     e.preventDefault();
@@ -8,9 +13,16 @@ searchBtn.addEventListener('click',async e=>{
     cityName = cityNameInput.value;
     if(cityName.length < 1) return;
     cityNameInput.value = '';
+    localStorage.setItem('cityName',cityName);
 
-    const fetchData = await fetch(`/weather/${cityName}`);
-    const weatherData = await fetchData.json();
+    const weatherData = await getWeatherData(cityName);
 
     populateTemplate(weatherData);
 });
+
+async function getWeatherData(cityName) {
+    const fetchData = await fetch(`/weather/${cityName}`);
+    const weatherData = await fetchData.json();
+
+    return weatherData;
+}
